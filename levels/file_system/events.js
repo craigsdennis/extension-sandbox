949265ed7623d91899ccbf47b977ee358720bcb4
Craig Dennis <cdennis@twilio.com>
2021-12-15 17:48:53 -0800

    Adds some file checking using chokidar

--- levels/file_system/events.js
+++ levels/file_system/events.js
@@ -1,5 +1,82 @@
-module.exports = function(event, world) {
-  console.log(`VR Template: ${event.name}`);
-  console.log(event);
-  console.log(world);
+window.reloadExternalModules = true;
+
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+
+const chokidar = require("chokidar");
+
+const INITIAL_STATE = {
+  nothingYet: false,
+};
+
+// Create a unique key for our state to be stored under
+const WORLD_STATE_KEY = "com.twilioquest.file_system";
+
+function getStartingDirectory() {
+  // TODO: let them set this
+  return path.resolve(os.homedir(), "tq-files");
 }
+
+function checkDirectoryState() {
+  // TODO: There could be multiple of these
+  const expectedPath = ["examples", "greetings", "ahoy.txt"];
+  // An array of paths one step at a time
+  const progressivePaths = expectedPath.reduce((accumulator, value) => {
+    let initialPath = getStartingDirectory();
+    if (accumulator.length > 0) {
+      initialPath = accumulator[accumulator.length - 1];
+    }
+    accumulator.push(path.resolve(initialPath, value));
+    return accumulator;
+  }, []);
+  const results = progressivePaths.map((filePath) => {
+    const exists = fs.existsSync(filePath);
+    return {
+      filePath,
+      exists,
+    };
+  });
+  const firstMissing = results.find((entry) => !entry.exists);
+  let message;
+  if (firstMissing !== undefined) {
+    // Hacky...check and see if the file has an extension
+    const isFile = path.basename(firstMissing.filePath).includes(".");
+    message = `Please add the ${isFile ? "file" : "directory"} ${firstMissing.filePath}`;
+  }
+  return {
+    valid: firstMissing === undefined,
+    message,
+    results,
+  };
+}
+
+module.exports = function (event, world) {
+  // Load our world state
+  const worldState = world.getState(WORLD_STATE_KEY) || INITIAL_STATE;
+
+  function updateDirectoryFeedback() {
+    const currentDirectoryState = checkDirectoryState();
+    if (!currentDirectoryState.valid) {
+      world.showNotification(currentDirectoryState.message);
+    } else {
+      world.showNotification("You did it!");
+      world.hideEntities("fire");
+      watcher.unwatch(getStartingDirectory());
+    }
+  }
+  
+  if (event.name === "mapDidLoad") {
+    world.showNotification(`Working directory is ${getStartingDirectory()}`)
+    const watcher = chokidar.watch(getStartingDirectory(), {
+      ignoreInitial: true,
+    });
+    watcher.on('all', (fileEvent, path) => {
+      console.log(fileEvent, path);
+      updateDirectoryFeedback();
+    });
+  }
+
+  updateDirectoryFeedback();
+  world.setState(WORLD_STATE_KEY, worldState);
+};
